395c64fec492ab03726672395ccc6fafd8b3de38
Tomar1234 <tomarshiva7017@gmail.com>
2026-04-24 15:55:34 +0530

    update/vite

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://api.autohubexpress.us",
         changeOrigin: true,
         secure: false,
       },
