9cb5a543d83923e9d9f68c52b223a44f7fe47d0b
Tomar1234 <tomarshiva7017@gmail.com>
2026-06-02 14:31:00 +0530

    fix-normal-email-format

--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -1174,13 +1174,19 @@ Thank you for reaching out.
 
                                                                             .replace(/<script[\s\S]*?<\/script>/gi, "")
 
-                                                                            .replace(/<\/(h1|h2|h3|h4|h5)>/gi, "\n\n")
+                                                                            .replace(/<\/(h1|h2|h3|h4|h5|p|div)>/gi, "\n\n")
 
-                                                                            .replace(/<\/(p|div|table|tr)>/gi, "\n\n")
+                                                                            .replace(/<\/(br|li)>/gi, "\n")
 
-                                                                            .replace(/<\/(br|li|td)>/gi, "\n")
+                                                                            .replace(/<\/(table|tr)>/gi, "\n")
+
+                                                                            .replace(/<\/(td>)/gi, " ")
 
                                                                             .replace(/\n{3,}/g, "\n\n")
+
+                                                                            .replace(/[ \t]+\n/g, "\n")
+
+                                                                            .replace(/\n\s+\n/g, "\n\n")
 
                                                                             // remove html
                                                                             .replace(/<[^>]+>/g, "")
