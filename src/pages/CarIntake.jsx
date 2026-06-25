f8db3d14d7fad9096a6e4703a4bb8dcd035c3c6a
Tomar1234 <tomarshiva7017@gmail.com>
2026-06-25 14:38:19 +0530

    update/Diagnosis

--- src/pages/CarIntake.jsx
+++ src/pages/CarIntake.jsx
@@ -1939,7 +1939,7 @@ const CarIntake = () => {
                   <div
                     className={`nav-link ${currentStep === 3 ? "active" : ""}`}
                   >
-                    <span className="step-number">03. Car Diagnosis</span>
+                    <span className="step-number">03. Car Diagnostic</span>
                   </div>
                 </li>
                 <li className="nav-item">
